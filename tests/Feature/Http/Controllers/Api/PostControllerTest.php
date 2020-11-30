0968215dc8615e80eb49c5fba98d7ef280dd254d
Hector Rubi <hector.rubi.garcia@outlook.com>
2020-11-29 22:39:18 -0600

    Make show method using TDD

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use App\Post;
+
 class PostControllerTest extends TestCase
 {
     use RefreshDatabase;
@@ -32,5 +34,22 @@ class PostControllerTest extends TestCase
 
         $response->assertStatus(422); // Estatus HTTP 422 - Fue imposible completarla
         $response->assertJsonValidationErrors('title');
+    }
+
+    public function testShow()
+    {
+        $post = factory(Post::class)->create();
+
+        $response = $this->json('GET', "/api/posts/$post->id"); // id =  1
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => $post->title])
+            ->assertStatus(200); // OK
+    }
+
+    public function test404Show()
+    {
+        $response = $this->json('GET', '/api/posts/1000');
+        $response->assertStatus(404); // Not found
     }
 }
